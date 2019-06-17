a734cb5849580f16820d21128940cef8ff91bd3a
Karly Mareka <karly.mareka@gmail.com>
2019-06-17 01:23:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+  get '/' do 
+    "Welcome to your app!!!! I built this! Testing 123... "
+  end
 
 
 end
